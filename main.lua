9390572f38dd15ea7515cb773fd18a9632f977cb
Peter <77168867+peterron03@users.noreply.github.com>
2025-07-07 15:26:32 -0500

    added various new functions

--- main.lua
+++ main.lua
@@ -1,22 +1,115 @@
 -- Utils --
 local Utils = {}
+local cachedBadgeInfo = {}
 
-function Utils.awardBadge(player : Player, badgeId : number) : boolean
+function Utils.isPrivateServer()
+	return game.PrivateServerId ~= "" and game.PrivateServerOwnerId ~= 0
+end
+
+function Utils.createSignal(eventName : string, remotes : Folder?) : RemoteEvent
+	local signal = Instance.new("RemoteEvent")
+	signal.Name = eventName
+	signal.Parent = remotes or game:GetService("ReplicatedStorage")
+	return signal
+end
+
+function Utils.lerpColor(color1 : Color3, color2 : Color3, alpha : number) : Color3
+	return Color3.new(
+		color1.R + (color2.R - color1.R) * alpha,
+		color1.G + (color2.G - color1.G) * alpha,
+		color1.B + (color2.B - color1.B) * alpha
+	)
+end
+
+function Utils.playTempSoundClone(sound : Sound, destroyAfter : number?)
+	local cloned = sound:Clone()
+	cloned.Parent = sound.Parent
+	cloned:Play()
+
+	task.delay(destroyAfter or 5, function()
+		cloned:Destroy()
+	end)
+end
+
+function Utils.getHRPAndHumanoidFromPlayer(player : Player) : (BasePart, Humanoid)
+	local character = player.Character
+	local hrp = character and character:FindFirstChild("HumanoidRootPart")
+	local humanoid = character and character:FindFirstChild("Humanoid")
+
+	return hrp, humanoid
+end
+
+function Utils.getRandomValuesFromArray(array : {any}, min : number, max : number, increasedChanceForValue : {value : string, chance : number}?) : {any}
+	if increasedChanceForValue then
+		local toAdd = math.round(#array * increasedChanceForValue.chance) - 1
+
+		if toAdd > 0 then
+			for i = 1, toAdd do
+				table.insert(array, increasedChanceForValue.value)
+			end
+		end
+	end
+	
+	local random = math.random(min, max)
+	local values = {}
+
+	for i = 1, random do
+		table.insert(values, array[math.random(1, #array)])
+	end
+
+	return values
+end
+
+function Utils.playerOwnsBadge(player : Player, badgeId : number) : boolean
 	local BadgeService = game:GetService("BadgeService")
 	
+	local success, result = pcall(function()
+		return BadgeService:UserHasBadgeAsync(player.UserId, badgeId)
+	end)
+	
+	if success then
+		return result
+	else
+		warn(result)
+		return false
+	end
+end
+
+function Utils.awardBadge(player : Player, badgeId : number, timeDelay : number?) : boolean
+	local BadgeService = game:GetService("BadgeService")
+	
+	if type(timeDelay) ~= "number" then timeDelay = 0 end
+	
 	local success, badgeInfo = pcall(function()
-		return BadgeService:GetBadgeInfoAsync(badgeId)
+		if not cachedBadgeInfo[badgeId] then
+			cachedBadgeInfo[badgeId] = BadgeService:GetBadgeInfoAsync(badgeId)
+		end
+		
+		return cachedBadgeInfo[badgeId]
 	end)
 
 	if success then
-		if badgeInfo.IsEnabled then
-			local awardSuccess, result = pcall(function()
-				return BadgeService:AwardBadge(player.UserId, badgeId)
-			end)
-			
-			return result and awardSuccess
-		end
+		task.delay(timeDelay or 0, function()
+			if badgeInfo.IsEnabled then
+				local awardSuccess, result = pcall(function()
+					if not Utils.playerOwnsBadge(player, badgeId) then
+						return BadgeService:AwardBadge(player.UserId, badgeId)
+					end
+				end)
+				
+				if not awardSuccess then
+					local success2, result2 = pcall(function()
+						return BadgeService:AwardBadge(player.UserId, badgeId)
+					end)
+					
+					return result2 and success2
+				end
+				
+				return result and awardSuccess
+			end
+		end)
 	else
+		cachedBadgeInfo[badgeId] = nil
 		warn("Error while fetching badge info: " .. badgeInfo)
 	end
 	
@@ -63,10 +156,11 @@ end
 
 function Utils.roundNumber(n : number, numberOfDecimalPlaces : number?) : number
 	local split = string.split(string.format("%." .. (numberOfDecimalPlaces or 0) .. "f", n), ".")
-	local newSplit = string.split(split[2], "")
+	local split2 = split[2] or "0"
+	local newSplit = string.split(split2, "")
 
-	while tostring(newSplit[#split[2]]) == "0" do
-		table.remove(newSplit, #split[2])
+	while tostring(newSplit[#split2]) == "0" do
+		table.remove(newSplit, #split2)
 	end
 
 	if newSplit and newSplit[1] then
@@ -90,6 +184,8 @@ function Utils.formatNumber(n : number, onlyIfHigherThan : number?, noCommas : b
 	local names = {"K","M","B","T","q","Q","s","S","O","N","d","U","D","t"}
 	local pows = {}
 	
+	onlyIfHigherThan = onlyIfHigherThan or 1000
+	
 	for i = 1, #names do table.insert(pows, 1000^i) end
 	
 	local ab = math.abs(n)
@@ -104,6 +200,23 @@ end
 
 function Utils.Format(n : number, formatString : string) : string
 	return string.format(formatString, n)
+end
+
+function Utils.convertToDHMS(sec : number, includeDecimals : boolean?) : string
+	local mins = (sec - sec % 60) / 60
+	sec = sec - mins * 60
+
+	local hrs = (mins - mins % 60) / 60
+	mins = mins - hrs * 60
+	
+	local days = (hrs - hrs % 24) / 24
+	hrs = hrs - days * 24
+
+	if includeDecimals then
+		return Utils.Format(days, "%02i") .. ":" .. Utils.Format(hrs, "%02i") .. ":" .. Utils.Format(mins, "%02i") .. ":" .. Utils.roundNumber(sec, 2)
+	else
+		return Utils.Format(days, "%02i") .. ":" .. Utils.Format(hrs, "%02i") .. ":" .. Utils.Format(mins, "%02i") .. ":" .. Utils.Format(sec, "%02i")
+	end
 end
 
 function Utils.convertToHMS(sec : number, includeDecimals : boolean?) : string
@@ -194,6 +307,20 @@ function Utils.isInGroupAsync(userId : number, groupId : number) : boolean?
 	end
 		
 	return false
+end
+
+function Utils.sendNotification(title : string, message : string, icon : string?, button1 : string?, button2 : string?, callback : BindableEvent?)
+	local StarterGui = game:GetService("StarterGui")
+	
+	StarterGui:SetCore("SendNotification", {
+		Title = title,
+		Text = message,
+		Duration = #message * 0.15,
+		Icon = icon,
+		Button1 = button1,
+		Button2 = button2,
+		Callback = callback
+	})
 end
 
 return Utils
